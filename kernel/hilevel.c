d403f8c03498a16b14fa0b20b1d8b8b852ba181c
Ethan <sk18181@bristol.ac.uk>
2020-04-17 21:20:56 +0100

    implemented counter

--- kernel/hilevel.c
+++ kernel/hilevel.c
@@ -6,6 +6,42 @@
  */
 
 #include "hilevel.h"
+#include <time.h>
+#include <stdlib.h>
+#include "SYS.h"
+#include <stdio.h>
+#include <string.h>
+
+ /* reverse:  reverse string s in place */
+ void reverse(char s[])
+ {
+     int i, j;
+     char c;
+ 
+     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+         c = s[i];
+         s[i] = s[j];
+         s[j] = c;
+     }
+ }
+ void itoa(int n, char s[])
+ {
+     int i, sign;
+ 
+     if ((sign = n) < 0)  /* record sign */
+         n = -n;          /* make n positive */
+     i = 0;
+     do {       /* generate digits in reverse order */
+         s[i++] = n % 10 + '0';   /* get next digit */
+     } while ((n /= 10) > 0);     /* delete it */
+     if (sign < 0)
+         s[i++] = '-';
+     s[i] = '\0';
+     reverse(s);
+ }
+
+
+
 
 void hilevel_handler_rst() {
   /* Configure the mechanism for interrupt handling by
@@ -35,14 +71,32 @@ void hilevel_handler_rst() {
    *   a software-interrupt (i.e., a trap or system call).
    */
 
+
+ 
+
+
+  int last=0;
   while( 1 ) {
-    for( int i = 0; i < 0x20000000; i++ ) {
+    for( int i = 0; i < 0x00100000; i++ ) {
       asm volatile( "nop" );
     }
+  
+    char numbers[33];
 
-    asm volatile( "svc 0" );
+    int anotherInteger = SYSCONF->COUNTER_24MHZ;
+
+
+    if(anotherInteger>last+24000000){
+          asm volatile( "svc 0" );
+  
+
+    PL011_putc( UART0, '\n',                      true ); 
+    last=anotherInteger;
+    }
+
+  
   }
-
+  
   return;
 }
 
@@ -79,7 +133,7 @@ void hilevel_handler_svc() {
    */
 
   PL011_putc( UART0, 'T', true );  
-
+  
   return;
 }
 
